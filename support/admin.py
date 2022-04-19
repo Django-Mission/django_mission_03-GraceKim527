9bacf68696bcbed615ad840a195484980bc15c5d
김은혜 <lemonherb0323@naver.com>
2022-04-19 18:19:25 +0900

    challenge mision complete

--- support/admin.py
+++ support/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-from .models import Faq
+from .models import Faq, Inquiry, Answer
 # Register your models here.
 
 @admin.register(Faq)
 class FaqModelAdmin(admin.ModelAdmin):
     list_display = ('id', 'question', 'category', 'answer', 'writer', 'date', 
     'final_modify', 'final_date')
+
+@admin.register(Inquiry)
+class InquiyModelAdmin(admin.ModelAdmin):
+    list_display = ('id','title', 'email', 'SMS', 'content', 'image')
+
+@admin.register(Answer)
+class AnswerModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'answer', 'content', 'ans_writer', 'ans_date',
+    'ans_final_mod', 'ans_final_date')
--- support/models.py
+++ support/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth import get_user_model
 User = get_user_model() #
 
 # Create your models here.
+
+# BASIC MISSION 
 class Faq(models.Model):
     question = models.TextField(verbose_name="질문")
     GENERAL = '일반'
@@ -20,4 +22,20 @@ class Faq(models.Model):
     writer = models.ForeignKey(to=User, on_delete=models.CASCADE, null=True, blank=True, verbose_name="생성자", related_name='writer')
     date = models.DateTimeField(verbose_name="생성일시", auto_now_add=True)
     final_modify = models.ForeignKey(to=User, on_delete=models.CASCADE, null=True, blank=True, default=None, verbose_name="최종 수정자", related_name='final_modify')
-    final_date = models.DateTimeField(verbose_name="최종 수정일시", auto_now=True)
+    final_date = models.DateTimeField(verbose_name = "최종 수정일시", auto_now = True)
+
+# ADVANCED MISSION
+class Inquiry(models.Model): #1대 1 모델
+    title = models.TextField(max_length = 50, verbose_name = "제목", null = True, blank = True)
+    email = models.CharField(max_length = 50, verbose_name = "이메일", null = True, blank = True)
+    SMS = models.CharField(max_length = 50, verbose_name = "문자메시지")
+    content = models.TextField(verbose_name = "내용")
+    image = models.ImageField(verbose_name = "이미지", null = True, blank = True)
+    
+class Answer(models.Model): #답변 모델
+    answer = models.TextField(verbose_name = "답변 내용")
+    content = models.ForeignKey(to = 'Inquiry', on_delete = models.CASCADE, verbose_name = "참조 문의글")
+    ans_writer = models.ForeignKey(to=User, on_delete = models.CASCADE, default = User, null = True, blank = True, verbose_name = "생성자", related_name = 'ans_writer')
+    ans_date = models.DateTimeField(verbose_name = "생성 일시", auto_now_add = True)
+    ans_final_mod = models.ForeignKey(to = User, on_delete = models.CASCADE, null = True, blank = True, default = None, verbose_name = "최종 수정자", related_name = 'ans_final_mod')
+    ans_final_date = models.DateTimeField(verbose_name = "최종 수정일시", auto_now = True)
